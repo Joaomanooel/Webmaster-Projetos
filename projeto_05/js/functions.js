de07e77cea05efac0f44a562428b5b3b1e4e4a04
João Manoel <64815276+Joaomanooel@users.noreply.github.com>
2023-01-11 09:57:46 -0300

    Slide
    criando slide na pagina individual de carro

--- projeto_05/js/functions.js
+++ projeto_05/js/functions.js
@@ -1,62 +1,44 @@
 $(function(){
-	var currentValue = 0;
-	var isDrag = false;
-	var preco_maximo = 70000;
-	var preco_atual = 0;
+	
+		/*
+			Sistema depesquisa
+        */
 
-	$('.pointer-barra').mousedown(function(){
-		isDrag = true;
-	})
+        var currentValue = 0;
+        var isDrag = false;
+        var preco_maximo = 70000;
+        var preco_atual = 0;
 
-	$(document).mouseup(function(){
-		isDrag = false;
-		enableTextSelection();
-	})
+        $('.pointer-barra').mousedown(function(){
+        	isDrag = true;
+        })
 
-	$('.barra-preco').mousemove(function(e){
-		if(isDrag){
-			disableTextSelection();
-			var elBase = $(this);
-			var mouseX = e.pageX - elBase.offset().left;
-			if(mouseX < 0)
-				mouseX = 0;
-			if(mouseX > elBase.width())
-				mouseX = elBase.width();
-			
-			$('.pointer-barra').css('left',(mouseX-13)+'px');
-			currentValue = (mouseX / elBase.width()) * 100;
-			$('.barra-preco-fill').css('width',currentValue+'%');
-			
-			preco_atual = (currentValue/100) * preco_maximo;
-			preco_atual = formatarPreco(preco_atual);
-			$('.preco_pesquisa').html('R$'+preco_atual);
-		}
-	})
+        $(document).mouseup(function(){
+        	isDrag = false;
+        	enableTextSelection();
+        })
 
-	function formatarPreco(preco_atual){
-		preco_atual = preco_atual.toFixed(2);
-		preco_arr = preco_atual.split('.');
+        $('.barra-preco').mousemove(function(e){
+        	if(isDrag){
+        		disableTextSelection();
+        		var elBase = $(this);
+        		var mouseX = e.pageX - elBase.offset().left;
+        		if(mouseX < 0)
+        			mouseX = 0;
+        		if(mouseX > elBase.width())
+        			mouseX = elBase.width();
 
-		var novo_preco = formatarTotal(preco_arr);
+        		$('.pointer-barra').css('left',(mouseX-13)+'px');
+        		currentValue = (mouseX / elBase.width()) * 100;
+        		$('.barra-preco-fill').css('width',currentValue+'%');
 
-		return novo_preco;
-	}
+        		//TODO: Ajustar o formato do preço!
+        		preco_atual = (currentValue/100) * preco_maximo;
+        		$('.preco_pesquisa').html('R$'+preco_atual);
+        	}
+        })
 
-	function formatarTotal(preco_arr){
-		
-		if(preco_arr[0] < 1000){
-			return preco_arr[0]+','+preco_arr[1];
-		}else if(preco_arr[0] < 10000){
-			return preco_arr[0][0]+'.'+preco_arr[0].substr(1,preco_arr[0].length)+
-			','+preco_arr[1];
-		}else{
-			return preco_arr[0][0]+preco_arr[0][1]+'.'+preco_arr[0].substr(2,preco_arr[0].length)+
-			','+preco_arr[1];
-		}
-
-	}
-
-	function disableTextSelection(){
+        function disableTextSelection(){
 	          $("body").css("-webkit-user-select","none");
 	          $("body").css("-moz-user-select","none");
 	          $("body").css("-ms-user-select","none");
@@ -64,27 +46,73 @@ $(function(){
 	          $("body").css("user-select","none");
         }
 
-    function enableTextSelection(){
+        function enableTextSelection(){
 	          $("body").css("-webkit-user-select","auto");
 	          $("body").css("-moz-user-select","auto");
 	          $("body").css("-ms-user-select","auto");
 	          $("body").css("-o-user-select","auto");
 	         $("body").css("user-select","auto");
         }
+		/*
+			Sistema individual do carro
+        */
+    	var imgShow = 3;
+    	var maxIndex = Math.ceil($('.mini-img-wraper').length/3) - 1;
+    	var curIndex = 0;
 
+    	initSlider();
+    	navigateSlider();
+    	clickSlider();
+    	function initSlider() {
+    		var amt =$('.mini-img-wraper').length * 33.3;
+    		var elScroll = $('.nav-galeria-wraper');
+    		var elSingle = $('.mini-img-wraper');
+    		elScroll.css('width',amt+'%');
+    		elSingle.css('width',33.3*(100/amt)+'%');
+    	}
 
-    var imgShow = 3;
-    var minIndex = imgShow - 1;
-    var maxIndex = Math.ceil($('.mini-img-wraper').length/3)-1;
-    var curIndex = 0;
+    	function navigateSlider(){
+    		$('.arrow-right-nav').click(function(){
+    			if(curIndex < maxIndex){
+    				curIndex++;
+    				var elOff = $('.mini-img-wraper').eq(curIndex*3).offset().left - $('.nav-galeria-wraper').offset().left;
+    				$('.nav-galeria').animate({'scrollLeft':elOff+'px'});
+    			}else{
 
+    			}
+    			
+    		});
+    		
+    		$('.arrow-left-nav').click(function(){
+    			if(curIndex > 0){
+    				curIndex--;
+    				var elOff = $('.mini-img-wraper').eq(curIndex*3).offset().left - $('.nav-galeria-wraper').offset().left;
+    				$('.nav-galeria').animate({'scrollLeft':elOff+'px'});
+    			}else{
+    			
+    			}
+    		});
+    	}
 
-    initSlider();
-    function initSlider(){
-    	var amt = $('.mini-img-wraper').length * 33.3;
-    	var elScroll = $('.nav-galeria-wraper');
-    	var elSingle = $('.mini-img-wraper');
-    	elScroll.css('widht', amt+'%');
-    	elSingle.css('width', 33.3*(100/amt)+'%');
-    }
+    	function clickSlider(){
+    		$('.mini-img-wraper').click(function(){
+    			$('.mini-img-wraper').css('background-color','transparent');
+    			$(this).css('background-color','rgb(210,210,210)');
+    			var img = $(this).children().css('background-image');
+    			$('.foto-destaque').css('background-image',img);
+    		})
+    		
+    		$('.mini-img-wraper').eq(0).click();
+    	}
+
+    	/*
+			Sistema contato
+        */
+
+    	$('[goto=contato]').click(function(){
+    		$('nav a').css('color','black');
+    		$(this).css('color','#EB2D2D');
+    		$('html,body').animate({'scrollTop':$('#contato').offset().top});
+    		return false;
+    	})
 })
